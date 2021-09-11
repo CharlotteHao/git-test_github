7db27ab07e08778affe274acc7109b54f5d6910a
shahao <1771478728@qq.com>
2021-09-11 20:49:46 +0800

    gitee commit

--- src/main/java/com/hao/git/GitApplication.java
+++ src/main/java/com/hao/git/GitApplication.java
@@ -14,6 +14,7 @@ public class GitApplication {
         System.out.println("git6");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
         SpringApplication.run(GitApplication.class,args);
     }
 }
