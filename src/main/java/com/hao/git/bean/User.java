fc8d7dcb0d4af646fd3af17bdbaf46e5983711b5
shahao <1771478728@qq.com>
2023-03-31 18:36:35 +0800

    idea git commit master test

--- src/main/java/com/hao/git/bean/User.java
+++ src/main/java/com/hao/git/bean/User.java
@@ -6,5 +6,6 @@ public class User {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
+        System.out.println("master test");
     }
 }
